9e30d98c60587fe071658445b0165dabab7f8348
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-01-16 18:36:39 +0100

    Add feature flag

--- basic_graph_implmentation/Program.cs
+++ basic_graph_implmentation/Program.cs
@@ -4,28 +4,48 @@ using basic_graph_implementation.Graphs;
 using basic_graph_implementation.NodeTypes;
 using System.Xml.Linq;
 
-var node1 = new SocialNode("Alice");
-var node2 = new SocialNode("Peter");
-var node3 = new SocialNode("John");
-var node4 = new SocialNode("Bart");
-
-var graph1 = new UndirectedGraph<SocialNode>();
-graph1.AddNode(node1);
-graph1.AddNode(node2);
-graph1.AddNode(node3);
-graph1.AddNode(node4);
-graph1.AddEdge(node1, node2);
-graph1.AddEdge(node2, node3);
-graph1.AddEdge(node3, node4);
-graph1.ShowGraph();
-//graph1.RemoveNode(node2);
-//Console.WriteLine("------------");
-//graph1.ShowGraph();
-
-var bfsResult = BreadthFirstSearch.BreadthFirstSearchStart(graph1, node1);
-
-Console.WriteLine("BFS Result:");
-foreach (var nodeId in bfsResult)
+class Program
 {
-    Console.WriteLine(nodeId);
+    static void Main(string[] args)
+    {
+        // Feature flags
+        var featureFlags = new Dictionary<string, bool>
+        {
+            { "EnableBFS", true } // Set to false to disable BFS execution
+        };
+
+        var node1 = new SocialNode("Alice");
+        var node2 = new SocialNode("Peter");
+        var node3 = new SocialNode("John");
+        var node4 = new SocialNode("Bart");
+
+        var graph1 = new UndirectedGraph<SocialNode>();
+        graph1.AddNode(node1);
+        graph1.AddNode(node2);
+        graph1.AddNode(node3);
+        graph1.AddNode(node4);
+        graph1.AddEdge(node1, node2);
+        graph1.AddEdge(node2, node3);
+        graph1.AddEdge(node3, node4);
+        graph1.ShowGraph();
+        //graph1.RemoveNode(node2);
+        //Console.WriteLine("------------");
+        //graph1.ShowGraph();
+
+        // Execute BFS only if the feature flag is enabled
+        if (featureFlags["EnableBFS"])
+        {
+            var bfsResult = BreadthFirstSearch.BreadthFirstSearchStart(graph1, node1);
+
+            Console.WriteLine("BFS Result:");
+            foreach (var nodeId in bfsResult)
+            {
+                Console.WriteLine(nodeId);
+            }
+        }
+        else
+        {
+            Console.WriteLine("BFS is disabled by feature flag.");
+        }
+    }
 }
