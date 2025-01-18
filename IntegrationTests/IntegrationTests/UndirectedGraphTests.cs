7bf0af9b9ce535b46cc3c48225b1d7a7c5c2e042
rejreno <82178724+rejreno@users.noreply.github.com>
2025-01-18 10:13:41 +0100

    Add new tests and exceptions (#6)
    Add Authors
    Add new tests and exceptions
    
    ---------
    
    Co-authored-by: Krzysztof Kokot <krzysiukokot@interia.pl>
    Co-authored-by: Rafal Juraś <rejreno@wp.pl>
    Co-authored-by: Krzysztof Kokot <45860442+KoKocik1@users.noreply.github.com>

--- IntegrationTests/IntegrationTests/UndirectedGraphTests.cs
+++ IntegrationTests/IntegrationTests/UndirectedGraphTests.cs
@@ -1,3 +1,4 @@
+using basic_graph_implementation.Exceptions;
 using basic_graph_implementation.Graphs;
 using basic_graph_implementation.Interfaces;
 namespace GraphImplementationTests.IntegrationTests
@@ -15,17 +16,22 @@ namespace GraphImplementationTests.IntegrationTests
         }
 
         [Fact]
-        public void ShowGraph_HappyPath()
+        public void CreateGraph_HappyPath()
         {
             // Arrange
             var graph = new UndirectedGraph<MockNode>();
-            var node1 = new MockNode("1");
-            var node2 = new MockNode("2");
+            var node1 = new MockNode("Robert");
+            var node2 = new MockNode("Peter");
+            var node3 = new MockNode("Andrew");
+            var node4 = new MockNode("Mick");
 
             // Act
             graph.AddNode(node1);
             graph.AddNode(node2);
+            graph.AddNode(node3);
+            graph.AddNode(node4);
             graph.AddEdge(node1, node2);
+            graph.AddEdge(node3, node4);
 
             // Assert
             var output = new StringWriter();
@@ -33,8 +39,79 @@ namespace GraphImplementationTests.IntegrationTests
 
             graph.ShowGraph();
 
-            var expected = "1 -- 2\r\n";
+            var expected = "Robert -- Peter\r\nAndrew -- Mick\r\n";
             Assert.Equal(expected, output.ToString());
+        }
+
+        [Fact]
+        public void AddNode_WhenNodeAlreadyExists_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var graph = new UndirectedGraph<MockNode>();
+            var node = new MockNode("Fred");
+            var node2 = new MockNode("Fred");
+
+            graph.AddNode(node);
+
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => graph.AddNode(node2));
+
+            Assert.Equal(GraphExceptions.NodeAlreadyExist, exception.Message);
+        }
+
+        [Fact]
+        public void AddEdge_WhenNodesDoNotExist_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var graph = new UndirectedGraph<MockNode>();
+            var node = new MockNode("Fred");
+            var node2 = new MockNode("Billy");
+            var node3 = new MockNode("John");
+
+            graph.AddNode(node);
+            graph.AddNode(node2);
+            graph.AddEdge(node, node2);
+
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => graph.AddEdge(node2, node3));
+
+            Assert.Equal(GraphExceptions.NodesDoNotExist, exception.Message);
+        }
+
+        [Fact]
+        public void AddEdge_WhenNodesAreTheSame_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var graph = new UndirectedGraph<MockNode>();
+            var node = new MockNode("Fred");
+            var node2 = new MockNode("Billy");
+
+            graph.AddNode(node);
+            graph.AddNode(node2);
+
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => graph.AddEdge(node2, node2));
+
+            Assert.Equal(GraphExceptions.NodesAreTheSame, exception.Message);
+        }
+
+        [Fact]
+        public void AddEdge_WhenEdgesAreDuplicated_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var graph = new UndirectedGraph<MockNode>();
+            var node = new MockNode("Fred");
+            var node2 = new MockNode("Billy");
+            var node3 = new MockNode("John");
+
+            graph.AddNode(node);
+            graph.AddNode(node2);
+            graph.AddEdge(node, node2);
+
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => graph.AddEdge(node, node2));
+
+            Assert.Equal(GraphExceptions.EdgesAreDuplicated, exception.Message);
         }
     }
 }
---
+++ basic_graph_implmentation/Exceptions/GraphExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_graph_implementation.Exceptions
+{
+    public static class GraphExceptions
+    {
+        public const string NodeAlreadyExist = "This node is already exists!";
+        public const string NodesDoNotExist = "Nodes dont exist!";
+        public const string NodesAreTheSame = "Nodes are the same!";
+        public const string EdgesAreDuplicated = "Edges are duplicated!";
+    }
+}
